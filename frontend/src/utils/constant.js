e1505264166891141414d7d1575de235404ff593
Arpit Shrivastava <165013375+arpit-shriv@users.noreply.github.com>
2026-04-12 00:37:03 +0530

    Update API endpoints to use new URLs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,11 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT =
+  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/user";
+
+export const JOB_API_END_POINT =
+  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/job";
+
+export const APPLICATION_API_END_POINT =
+  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/application";
+
+export const COMPANY_API_END_POINT =
+  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/company";
