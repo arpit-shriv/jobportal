17d1ac5ae56ff13509dddc21b4ce93ec8241c902
Arpit Shrivastava <165013375+arpit-shriv@users.noreply.github.com>
2026-04-12 01:29:31 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,11 +1,6 @@
-export const USER_API_END_POINT =
-  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/user";
+const BASE_URL = "https://jobportal--arpitshriv1023.replit.app/api/v1";
 
-export const JOB_API_END_POINT =
-  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/job";
-
-export const APPLICATION_API_END_POINT =
-  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/application";
-
-export const COMPANY_API_END_POINT =
-  "https://bafc69b0-dfea-47b5-80ab-680146e11f27-00-nyxyjjb4r9cd.sisko.replit.dev/api/v1/company";
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
